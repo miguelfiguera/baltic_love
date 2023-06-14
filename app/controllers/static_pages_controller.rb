e2a81b8ed27f7c9bfb3418f668e13ae097b477f6
Miguel Figuera <miguelqui725@gmail.com>
2023-06-14 01:43:52 -0400

    fix redirect to root and now redirects to dashboard

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,12 +1,8 @@
 class StaticPagesController < ApplicationController
   before_action :authenticate_admin!, only: [:index]
 
-  def home
-  end
+  def home; end
 
-  def features
-  end
+  def features;end
 
-  def dashboard
-  end
 end
