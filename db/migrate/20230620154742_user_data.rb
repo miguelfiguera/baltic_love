aa0c5883edbe38c2d42420ce846e857fc95a1bef
Miguel Figuera <miguelqui725@gmail.com>
2023-06-20 12:35:55 -0400

    user new migration

--- db/migrate/20230620154742_user_data.rb
+++ db/migrate/20230620154742_user_data.rb
@@ -1,4 +1,13 @@
 class UserData < ActiveRecord::Migration[7.0]
   def change
+    create_table :users do |t|
+      t.string :email, null: false
+      t.string :name, null: false
+      t.string :last_name, null: false
+      t.string :phone, null: false
+      t.string :country, default:'USA'
+      t.string :package, null: false
+      t.text :question
+    end
   end
 end
