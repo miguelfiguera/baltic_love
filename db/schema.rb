8bb50fdcf5099f0dc4a8404b36cb6c8a17b90514
Miguel Figuera <miguelqui725@gmail.com>
2023-06-20 12:47:52 -0400

    add new table to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_20_142545) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_20_154742) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_20_142545) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "name", null: false
+    t.string "last_name", null: false
+    t.string "phone", null: false
+    t.string "country", default: "USA"
+    t.string "package", null: false
+    t.text "question"
   end
 
 end
